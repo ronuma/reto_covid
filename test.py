bbca2ba108d69f6db5d78f43162c3bff671204bc
ronuma <ronum.00@gmail.com>
2022-03-11 12:06:23 -0600

    graficar
    Listo, ya hace todo, falta que opere con valores reales pero ya

--- test.py
+++ test.py
@@ -9,14 +9,21 @@ rawData = [
     "1|0|0|0|0|0|1|0|36.22|88.56|1|1|0|0|0",
 ]
 
-# bloque PYTHON para tomar los valores y crear arreglos
+# bloque PYTHON para graficar
 delimiter = "|"
 oxig = []
 temp = []
 
+# itera en los datos, extrae los valores y los pasa a sus arreglos correspondientes
 for read in rawData:
     read = read.split(delimiter)
     oxig.append(read[9])
     temp.append(read[8])
 
+# convierte los valores de los arreglos a float porque vienen como strings
+oxig = [float(x) for x in oxig]
+temp = [float(x) for x in temp]
+
+# se crean las graficas
 graph.create(oxig, "Oxigenacion")
+graph.create(temp, "Temperatura")
